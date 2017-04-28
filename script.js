4b0e6ce30c4e4d567ea49a6bc29af9ba1a0cb4af
uniquepassivegoface991 <uniquepassivegoface991@gmail.com>
2017-04-28 08:03:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $("#el1").click(function(){
     $(this).hide();
     $("#el2").show();
-    alert("Twój komputer został zainfekowany, prosimy nie opuszczać tej strony!");
+    for(var i=1;i<3;i++){
+      alert("Twój komputer został zainfekowany, prosimy nie opuszczać tej strony!");
+    }
   });
 });
