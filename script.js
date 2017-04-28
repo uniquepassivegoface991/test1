dee3408d5a282f6878493ac7e8ebcf5c91978c92
uniquepassivegoface991 <uniquepassivegoface991@gmail.com>
2017-04-28 08:14:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ $(document).ready(function(){
   var imgWidth = image.ClientWidth;
   var imgHeight = image.ClientHeight;
   
-  $("el2").css("top", 50%-imgHeight);
-  $("el2").css("left", 50%-imgWidth);
+  $("el2").css("top", calc(50%-imgHeight));
+  $("el2").css("left", calc(50%-imgWidth));
   
   $("#el1").click(function(){
     $(this).hide();
     $("#el2").show();
-      alert("Twój komputer został zainfekowany, prosimy nie opuszczać tej strony!");
-      alert("Twój komputer został zainfekowany, prosimy nie opuszczać tej strony!");
       alert("Twój komputer został zainfekowany, prosimy nie opuszczać tej strony!");
   });
 });
