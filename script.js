f2ad25f39c6728961349241d179e6e5c7c0bcd36
uniquepassivegoface991 <uniquepassivegoface991@gmail.com>
2017-04-28 08:34:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 $(document).ready(function(){
   var image = document.getElementById("el2");
-  var imgWidth = $("image").width();
+ /* var imgWidth = $("image").width();
   var imgHeight = $("image").height();
   var bodyWidth = $("document").width();
   var bodyHeight = $("document").height();
   
   $("el2").css("margin-top", bodyHeight/2-imgHeight);
-  $("el2").css("margin-left", bodyWidth/2-imgWidth);
+  $("el2").css("margin-left", bodyWidth/2-imgWidth); */
   $("#el1").click(function(){
     $(this).hide();
     $("#el2").show();
